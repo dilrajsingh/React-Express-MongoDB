6e64456d3a9d6932e30575ddf8437b9351093ce0
Dilraj Singh <dilraj.singh@mail.utoronto.ca>
2018-11-12 08:47:49 -0500

    added in GoogleStrategy for OAuth and the passport package.

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const express = require('express'); 
+const express = require('express');
 
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const keys = require('./config/keys')
 const app = express();
 
-const keys = require.apply('./config/keys')
-
- 
-// This was just for testing, I'll leave it in for fun
-app.get('/', (req, res) => {
-    res.send({ test: 'testing change 2'});
-});
-
-
-// creates a new instance of the Google strategy
-/* passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback' // route handler will be added here
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
     }, (accessToken) => {
         console.log(accessToken);
+ })
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google ', {
+    scope: ['profile', 'email']
     })
-); 
-*/
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
