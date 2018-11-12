18af4343516aea6fe0b4968ef53a4bf1c1420084
Dilraj Singh <dilraj.singh@mail.utoronto.ca>
2018-11-11 23:31:27 -0500

    Added in logic for Google+ OAuth using Passport.js.

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express'); 
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
+const keys = require.apply('./config/keys')
+
+ 
+// This was just for testing, I'll leave it in for fun
 app.get('/', (req, res) => {
-    res.send({ test: 'testing change'});
+    res.send({ test: 'testing change 2'});
 });
+
+
+// creates a new instance of the Google strategy
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback' // route handler will be added here
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
+); 
+
+// route user is sent to when granted permission
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
