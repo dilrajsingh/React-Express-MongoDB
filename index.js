d4b5d5693e8d081771f041cbdac223baffe6880e
Dilraj Singh <dilraj.singh@mail.utoronto.ca>
2018-11-14 22:10:46 -0500

    completed the authentication flow including the use of a cookie session.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const keys = require('./config/keys');
 
 require('./models/User'); // order matters!
@@ -9,6 +13,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+// code for cookies
+app.use(
+    cookieSession({ // configuration object
+        maxAge: 30 * 24 * 60 * 60 * 1000, // cookie to last 30 days, kept in milliseconds
+        keys: [keys.cookieKey] // cookie encrypted
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
