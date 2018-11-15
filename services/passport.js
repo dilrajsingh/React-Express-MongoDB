7d2beeec3a85bb94233125c6f65c3ab2aa99ae74
Dilraj Singh <dilraj.singh@mail.utoronto.ca>
2018-11-14 19:43:27 -0500

    added passport's done statements when checking if a user exists or adding one.

--- services/passport.js
+++ services/passport.js
@@ -13,10 +13,26 @@ passport.use(new GoogleStrategy(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refersh token', refreshToken);
-        console.log('profile', profile);
-       new User({ googleId: profile.id }).save() // new instance of a user, .save() saves it to db
+        // async action, this returns a promise
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                // record found
+                if (existingUser) {
+                    
+                    // tells passport that we are finished here
+                    done(null, existingUser);
+                }
+                //create user
+                else {
+                    // new model instance of a user, .save() saves it to db
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user)); // do not call done until the async operation completes
+                }
+            });
+        // console.log('access token', accessToken);
+        // console.log('refersh token', refreshToken);
+        // console.log('profile', profile);
     }
  )
 );
