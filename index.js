abbb2343f6bca5cade3f46a4d2308024f7708b25
OFFICE\dsing16 <dilraj.singh@bmo.com>
2018-11-21 16:20:46 -0500

    Heroku/production code set up.

--- index.js
+++ index.js
@@ -31,5 +31,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+// if in  production / heroku, order matters
+if (process.env.NODE_ENV === 'production'){
+    // Express will serve up Production Assets like the main.js/main.css file here
+    app.use(express.static('client/build'));
+    // Express will serve up the index.html file if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
