a83d63eb4fea1bc2142d04963c6b119a328ab2ac
Dilraj Singh <dilraj.singh@mail.utoronto.ca>
2018-11-20 19:25:16 -0500

    implemented stripe, stripe logic and body-parser to parse post request. Updated routes to handle billing.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 
+const bodyParser = require('body-parser');
+
 const keys = require('./config/keys');
 
 require('./models/User'); // order matters!
@@ -14,7 +16,9 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// middlewares:
 // code for cookies
+app.use(bodyParser.json()); // used to parse POST requests
 app.use(
     cookieSession({ // configuration object
         maxAge: 30 * 24 * 60 * 60 * 1000, // cookie to last 30 days, kept in milliseconds
@@ -25,7 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/billingRoutes.js
@@ -0,0 +1,18 @@
+// npmjs.com/package/stripe -> Documentation
+//  example: var stripe = require("stripe")("sk_test_CMOk7Zw6xQRBDUyvzGhK08Ab");
+
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+module.exports = (app) => {
+    app.post('/api/stripe', async (req, res) => {
+        // console.log(req.body);
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: '$5 for 5 credits',
+            source: req.body.id
+        });
+
+        console.log(charge);
+    });
+};
