925d3f7b750ddb2e38f981616334c2c48e6bb0df
OFFICE\dsing16 <dilraj.singh@bmo.com>
2018-11-22 12:50:20 -0500

    added to surveyRoutes the middleware requireLogin.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,9 @@
-module.exports = (app) => {
+const requireLogin = require('../middlewares/requireLogin');
 
+// check if user is logged in
+// check if user has enough credits, needs at least one
+module.exports = app => {
+    app.post('/api/surveys', requireLogin, (req, res) => {
+
+    });
 };
