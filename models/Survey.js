94871f12d1071ccd480ce6f8c295e8ee81f1fe87
OFFICE\dsing16 <dilraj.singh@bmo.com>
2018-11-22 12:09:22 -0500

    Added to the Survey Model a _user unique identifier tied to the User model and dateSent as well as lastResponded field.

--- models/Survey.js
+++ models/Survey.js
@@ -3,13 +3,19 @@ const { Schema } = mongoose;
 
 const RecipientSchema = require('./Recipient');
 
+/* Every survey belongs to a particular user.
+*/
+
 const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
     recipients: [RecipientSchema],
     yes: { type: Number, default: 0}, // Records vote
-    no: { type: Number, default: 0} // Records vote
+    no: { type: Number, default: 0}, // Records vote
+    _user: { type: Schema.Types.ObjectId, ref: 'User' }, // Convention is that _ is a reference field
+    dateSent: Date,
+    lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
