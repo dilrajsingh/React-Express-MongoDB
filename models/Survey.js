dcea2429b34a46ce4191aefa5163275dec0f857d
OFFICE\dsing16 <dilraj.singh@bmo.com>
2018-11-22 11:54:24 -0500

    Added to Survey model a yes/no vote.

--- models/Survey.js
+++ models/Survey.js
@@ -5,7 +5,9 @@ const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
-    recipients: [String] // array of strings to send
+    recipients: [String], // array of strings to send to user emails
+    yes: { type: Number, default: 0}, // Records vote
+    no: { type: Number, default: 0} // Records vote
 });
 
 mongoose.model('surveys', surveySchema);
