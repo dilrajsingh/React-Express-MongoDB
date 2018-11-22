d0321da57979403832c4d1454638011469251960
OFFICE\dsing16 <dilraj.singh@bmo.com>
2018-11-22 16:44:14 -0500

    BUG fixed 'send' to 'sendgrid'.

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,13 +1,14 @@
 // convention is capital for classes that export, passport doesn't export so it's lowercase 
 
-const send = require('sendgrid');
+const sendgrid = require('sendgrid');
 const helper = sendgrid.mail; // send grid documentation
 const keys = require('../config/keys');
 
 // from "Mail class" we want to add some additional features. Thus, extended/inherited
 class Mailer extends helper.Mail {
+    constructor() {
 
-
+    }
 }
 
 module.exports = Mailer;
