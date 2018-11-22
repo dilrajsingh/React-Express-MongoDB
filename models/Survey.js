db4a63830c8329acf2ba2e24591d4e06ebe57781
OFFICE\dsing16 <dilraj.singh@bmo.com>
2018-11-22 12:02:17 -0500

    created subdocument record instead of an array of Strings for the recipients in the Survey model.

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const RecipientSchema = require('./Recipient');
+
 const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
-    recipients: [String], // array of strings to send to user emails
+    recipients: [RecipientSchema],
     yes: { type: Number, default: 0}, // Records vote
     no: { type: Number, default: 0} // Records vote
 });
